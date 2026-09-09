e0cc07acef398a9fcad5194aabd7b7a2a4ed0465
m.numashita <m-numashita@insel.jp>
2026-09-09 16:59:10 +0900

    update

--- src/main.cpp
+++ src/main.cpp
@@ -6,33 +6,39 @@
 
 #include "randomManager.h"
 
-enum class EffectType{
-    Attack      ,//攻撃
-    Heal        ,//回復
+enum class EffectType
+{
+  Attack, // 攻撃
+  Heal,   // 回復
 };
 
-enum class ParamType{
-    Physical    ,//物理
-    Magic       ,//魔法
+enum class ParamType
+{
+  Physical, // 物理
+  Magic,    // 魔法
 };
 
-enum class TargetSide{
-    Enemy       ,//敵
-    Friend      ,//味方
-    Self        ,//自身
+enum class TargetSide
+{
+  Enemy,  // 敵
+  Friend, // 味方
+  Self,   // 自身
 };
 
-enum class TargetSelect{
-    LowestHp    ,//Hpが低い
-    Random      ,//ランダム
+enum class TargetSelect
+{
+  LowestHp, // Hpが低い
+  Random,   // ランダム
 };
 
-enum class TargetRange{
-    All         ,//全体
-    One         ,//単体
+enum class TargetRange
+{
+  All, // 全体
+  One, // 単体
 };
 
-class Action{
+class Action
+{
 private:
   EffectType mEffectType;
   ParamType mParamType;
@@ -40,72 +46,110 @@ private:
   std::optional<TargetSelect> mTargetSelect;
   TargetRange mTargetRange;
   unsigned int mPercent;
+
 public:
   Action(
-    EffectType et, 
-    ParamType pt, 
-    TargetSide ts, 
-    std::optional<TargetSelect> tse, 
-    TargetRange tr, 
-    unsigned int per):
-    mEffectType(et),
-    mParamType(pt),
-    mTargetSide(ts),
-    mTargetSelect(tse),
-    mTargetRange(tr),
-    mPercent(per)
-    {
-    }
+      EffectType et,
+      ParamType pt,
+      TargetSide ts,
+      std::optional<TargetSelect> tse,
+      TargetRange tr,
+      unsigned int per) : mEffectType(et),
+                          mParamType(pt),
+                          mTargetSide(ts),
+                          mTargetSelect(tse),
+                          mTargetRange(tr),
+                          mPercent(per)
+  {
+  }
 };
 
 class ActionTable
 {
 private:
   std::vector<std::unique_ptr<Action>> mTable;
+
 public:
   ActionTable() = default;
-  ActionTable( ActionTable&& a)
+  ActionTable(ActionTable &&a)
   {
     mTable = std::move(a.mTable);
   }
-  void AddAction( std::unique_ptr<Action> action )
+  void AddAction(std::unique_ptr<Action> action)
   {
-    mTable.push_back( std::move( action ) );
+    mTable.push_back(std::move(action));
   }
 };
 
-enum class ActionId{
-    AttackOne       ,//単体攻撃
-    AttackDouble    ,//２回攻撃
-    MagicAttackOne  ,//単体魔法攻撃
-    MagicAttackAll  ,//全体魔法攻撃
-    HealOne         ,//単体回復
-    HealAll         ,//全体回復
+enum class ActionId
+{
+  AttackOne,      // 単体攻撃
+  AttackDouble,   // ２回攻撃
+  MagicAttackOne, // 単体魔法攻撃
+  MagicAttackAll, // 全体魔法攻撃
+  HealOne,        // 単体回復
+  HealAll,        // 全体回復
 };
 
-class CharacterAction{
-    ActionId mId;
-    int mPercent;
+class CharacterAction
+{
+  ActionId mId;
+  int mPercent;
 };
 
-class Character{
+enum class CharacterType
+{
+  Warrior, // 戦士
+  Caster,  // 魔法使い
+  Healer,  // 回復役
+};
+
+class Character
+{
 private:
-    int mHp;
-    int mMaxHp;
-    int mAttack;
-    int mMagic;
-    int mDefence;
-    int mSpeed;
-    std::vector<CharacterAction> mAction;
+  CharacterType mType;
+  int mHp;
+  int mMaxHp;
+  int mAttack;
+  int mMagic;
+  int mDefence;
+  int mSpeed;
+  std::vector<CharacterAction> mAction;
+
 public:
-    Character( int maxHp, int attack, int magic, int defence, int speed ):
-        mHp(maxHp),mMaxHp(maxHp),mAttack(attack),mMagic(magic),mDefence(defence),mSpeed(speed)
-        {
-
-        }
+  Character(
+      CharacterType type,
+      int maxHp,
+      int attack,
+      int magic,
+      int defence,
+      int speed)
+      : mType(type),
+        mHp(maxHp),
+        mMaxHp(maxHp),
+        mAttack(attack),
+        mMagic(magic),
+        mDefence(defence),
+        mSpeed(speed)
+  {
+  }
+  int GetMaxHp() const
+  {
+    return mMaxHp;
+  }
+  int GetHp() const
+  {
+    return mHp;
+  }
+  int GetAttack() const
+  {
+    return mAttack;
+  }
+  int GetMagic() const
+  {
+    return mMagic;
+  }
 };
-
-
 
 // class MenuItemBase
 // {
@@ -139,100 +183,128 @@ public:
 //     void Exec(){}
 // };
 
+std::unique_ptr<Character> CreateCharacter(const unsigned int randomBase)
+{
+  std::unique_ptr<Character> character = std::make_unique<Character>(
+      CharacterType::Warrior,
+      50, // int maxHp,
+      0,  // int attack,
+      0,  // int magic,
+      0,  // int defence,
+      0   // int speed)
+  );
+  return character;
+}
+
+void DisplayCharacter(const Character &c)
+{
+  std::cout << "MaxHp:" << c.GetMaxHp() << "\n";
+}
 
 int main()
 {
-  std::vector< std::unique_ptr<ActionTable>> actionTable;
+  std::vector<std::unique_ptr<ActionTable>> actionTable;
 
-   //単体攻撃
-   std::unique_ptr<Action> actionAttack = std::make_unique<Action>(
-     EffectType::Attack,//攻撃
-     ParamType::Physical,//物理
-     TargetSide::Enemy,//敵
-     TargetSelect::LowestHp,//HPが一番低い
-     TargetRange::One,//1体
-     100
-     );
+  // 単体攻撃
+  std::unique_ptr<Action> actionAttack = std::make_unique<Action>(
+      EffectType::Attack,     // 攻撃
+      ParamType::Physical,    // 物理
+      TargetSide::Enemy,      // 敵
+      TargetSelect::LowestHp, // HPが一番低い
+      TargetRange::One,       // 1体
+      100);
 
   std::unique_ptr<ActionTable> actionTableAttack = std::make_unique<ActionTable>();
-  (*actionTableAttack).AddAction( std::move( actionAttack ) );
-  actionTable.push_back( std::move( actionTableAttack ) );
+  (*actionTableAttack).AddAction(std::move(actionAttack));
+  actionTable.push_back(std::move(actionTableAttack));
 
-  //２回攻撃
+  // ２回攻撃
   std::unique_ptr<Action> actionAttackDouble0 = std::make_unique<Action>(
-      EffectType::Attack,//攻撃
-      ParamType::Physical,//物理
-      TargetSide::Enemy,//敵
-      TargetSelect::Random,//ランダム
-      TargetRange::One,//1体
-      50
-      );
+      EffectType::Attack,   // 攻撃
+      ParamType::Physical,  // 物理
+      TargetSide::Enemy,    // 敵
+      TargetSelect::Random, // ランダム
+      TargetRange::One,     // 1体
+      50);
   std::unique_ptr<Action> actionAttackDouble1 = std::make_unique<Action>(
-      EffectType::Attack,//攻撃
-      ParamType::Physical,//物理
-      TargetSide::Enemy,//敵
-      TargetSelect::Random,//ランダム
-      TargetRange::One,//1体
-      50
-      );
+      EffectType::Attack,   // 攻撃
+      ParamType::Physical,  // 物理
+      TargetSide::Enemy,    // 敵
+      TargetSelect::Random, // ランダム
+      TargetRange::One,     // 1体
+      50);
   std::unique_ptr<ActionTable> actionTableAttackDouble = std::make_unique<ActionTable>();
-  (*actionTableAttackDouble).AddAction( std::move( actionAttackDouble0 ) );
-  (*actionTableAttackDouble).AddAction( std::move( actionAttackDouble1 ) );
-  actionTable.push_back( std::move( actionTableAttackDouble ) );
+  (*actionTableAttackDouble).AddAction(std::move(actionAttackDouble0));
+  (*actionTableAttackDouble).AddAction(std::move(actionAttackDouble1));
+  actionTable.push_back(std::move(actionTableAttackDouble));
 
-  //単体魔法攻撃
+  // 単体魔法攻撃
   std::unique_ptr<Action> actionAttackMagic = std::make_unique<Action>(
-      EffectType::Attack,//攻撃
-      ParamType::Magic,//魔法
-      TargetSide::Enemy,//敵
-      TargetSelect::LowestHp,//Hpが一番低い
-      TargetRange::One,//1体
-      100
-      );
+      EffectType::Attack,     // 攻撃
+      ParamType::Magic,       // 魔法
+      TargetSide::Enemy,      // 敵
+      TargetSelect::LowestHp, // Hpが一番低い
+      TargetRange::One,       // 1体
+      100);
   std::unique_ptr<ActionTable> actionTableAttackMagic = std::make_unique<ActionTable>();
-  (*actionTableAttackMagic).AddAction( std::move( actionAttackMagic ) );
-  actionTable.push_back( std::move( actionTableAttackMagic ) );
+  (*actionTableAttackMagic).AddAction(std::move(actionAttackMagic));
+  actionTable.push_back(std::move(actionTableAttackMagic));
 
-  //全体魔法攻撃
+  // 全体魔法攻撃
   std::unique_ptr<Action> actionAttackMagicAll = std::make_unique<Action>(
-      EffectType::Attack,//攻撃
-      ParamType::Magic,//魔法
-      TargetSide::Enemy,//敵
-      std::nullopt,//なし
-      TargetRange::All,//全体
-      33
-      );
+      EffectType::Attack, // 攻撃
+      ParamType::Magic,   // 魔法
+      TargetSide::Enemy,  // 敵
+      std::nullopt,       // なし
+      TargetRange::All,   // 全体
+      33);
   std::unique_ptr<ActionTable> actionTableAttackMagicAll = std::make_unique<ActionTable>();
-  (*actionTableAttackMagicAll).AddAction( std::move( actionAttackMagicAll ) );
-  actionTable.push_back( std::move( actionTableAttackMagicAll ) );
+  (*actionTableAttackMagicAll).AddAction(std::move(actionAttackMagicAll));
+  actionTable.push_back(std::move(actionTableAttackMagicAll));
 
-  //単体回復
+  // 単体回復
   std::unique_ptr<Action> actionHeal = std::make_unique<Action>(
-      EffectType::Heal,//回復
-      ParamType::Magic,//魔法
-      TargetSide::Friend,//味方
-      TargetSelect::LowestHp,//Hpが一番低い
-      TargetRange::One,//1体
-      100
-      );
+      EffectType::Heal,       // 回復
+      ParamType::Magic,       // 魔法
+      TargetSide::Friend,     // 味方
+      TargetSelect::LowestHp, // Hpが一番低い
+      TargetRange::One,       // 1体
+      100);
   std::unique_ptr<ActionTable> actionTableHeal = std::make_unique<ActionTable>();
-  (*actionTableHeal).AddAction( std::move( actionHeal ) );
-  actionTable.push_back( std::move( actionTableHeal ) );
+  (*actionTableHeal).AddAction(std::move(actionHeal));
+  actionTable.push_back(std::move(actionTableHeal));
 
-   //全体回復
-   std::unique_ptr<Action> actionHealAll = std::make_unique<Action>(
-       EffectType::Heal,//回復
-       ParamType::Magic,//魔法
-       TargetSide::Friend,//味方
-       std::nullopt,//なし
-       TargetRange::All,//全体
-       33
-       );
+  // 全体回復
+  std::unique_ptr<Action> actionHealAll = std::make_unique<Action>(
+      EffectType::Heal,   // 回復
+      ParamType::Magic,   // 魔法
+      TargetSide::Friend, // 味方
+      std::nullopt,       // なし
+      TargetRange::All,   // 全体
+      33);
   std::unique_ptr<ActionTable> actionTableHealAll = std::make_unique<ActionTable>();
-  (*actionTableHealAll).AddAction( std::move( actionHealAll ) );
-  actionTable.push_back( std::move( actionTableHealAll ) );
+  (*actionTableHealAll).AddAction(std::move(actionHealAll));
+  actionTable.push_back(std::move(actionTableHealAll));
 
-  
-  std::cout<<"Character Manager\n";
+  // キャラクター
+  RandomManager randCharacter(12345);
+  std::vector<std::unique_ptr<Character>> character;
+  for (int i = 0; i < 6; i++)
+  {
+    unsigned int rnd = randCharacter.Get();
+    std::cout << rnd << "\n";
+    character.push_back(CreateCharacter(rnd));
+  }
+  // std::unique_ptr<Character> c0 = std::make_unique<Character>(
+  //     CharacterType::Warrior,
+  //     50, // int maxHp,
+  //     0,  // int attack,
+  //     0,  // int magic,
+  //     0,  // int defence,
+  //     0   // int speed)
+  // );
+  // character.push_back(std::move(c0));
+
+  std::cout << "Character Manager\n";
   return 0;
 }
